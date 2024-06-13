7775d96656d71191dbabbb88ba99f3ab26065887
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-06-14 00:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", async ()=>{
+	const text = document.getElementById("text").value;
+	const delay = document.getElementById("delay").value;
+	if(!text || isNaN(delay)){
+		alert("Please fill in all fields and provide a value delaya and text.");
+		return;
+	}
+	await delayFunction(delay);
+	document.getElementById("outputText").innerText=text;
+});
+function delayFuction(delay) {
+	return new Promise((resolve, reject)=>{
+		setTimeout(resolve, delay);
+	});
+};
